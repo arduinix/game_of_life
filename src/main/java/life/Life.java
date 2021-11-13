cd29027c9e8454809dcaa2d9dc225e9c0ce02ad7
Ian Gagorik <ian.gagorik@gmail.com>
2021-11-13 12:03:59 -0800

    added ability to check a cell's neighbors
    Co-authored-by: Arduinix <arduinix88@gmail.com>

--- src/main/java/life/Life.java
+++ src/main/java/life/Life.java
@@ -4,6 +4,14 @@ import java.util.LinkedList;
 
 public class Life {
     public LinkedList<Cell> universe;
+    public Cell northOffset = new Cell(0, 1);
+    public Cell southOffset = new Cell(0, -1);
+    public Cell eastOffset = new Cell(1, 0);
+    public Cell westOffset = new Cell(-1, 0);
+    public Cell nwOffset = new Cell(-1, 1);
+    public Cell neOffset = new Cell(1, 1);
+    public Cell swOffset = new Cell(-1, -1);
+    public Cell seOffset = new Cell(1, -1);
 
     public Life() {
         this.universe = new LinkedList<Cell>();
@@ -15,5 +23,14 @@ public class Life {
 
     public void tick() {
         this.universe = new LinkedList<Cell>();
+    }
+
+    public boolean checkNeighbor(Cell inputCell, Cell offset) {
+        for (Cell cell : universe) {
+            if (cell.x == inputCell.x + offset.x && cell.y == inputCell.y + offset.y) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/life/LifeTest.java
+++ src/test/java/life/LifeTest.java
@@ -3,8 +3,10 @@ package life;
 import org.junit.Test;
 import java.util.LinkedList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LifeTest {
+    // Universe Tests
     @Test
     public void newUniverseIsEmpty() {
         Life life = new Life();
@@ -39,5 +41,40 @@ public class LifeTest {
         life.tick();
 
         assertEquals(life.universe, expectedUniverse);
+    }
+
+    // Neighbor Tests
+    @Test
+    public void canVerifyAllCellNeighbors() {
+        Cell liveCell_1_1 = new Cell(1, 1);
+        Cell northCell = new Cell(1, 2);
+        Cell southCell = new Cell(1 ,0);
+        Cell eastCell = new Cell(2, 1);
+        Cell westCell = new Cell(0, 1);
+        Cell nwCell = new Cell(2, 0);
+        Cell neCell = new Cell(2, 2);
+        Cell swCell = new Cell(0, 0);
+        Cell seCell = new Cell(0, 2);
+        LinkedList<Cell> universe = new LinkedList<Cell>();
+        universe.add(liveCell_1_1);
+        universe.add(northCell);
+        universe.add(southCell);
+        universe.add(eastCell);
+        universe.add(westCell);
+        universe.add(nwCell);
+        universe.add(neCell);
+        universe.add(swCell);
+        universe.add(seCell);
+
+        Life life = new Life(universe);
+
+        assertEquals(true, life.checkNeighbor(liveCell_1_1, life.northOffset));
+        assertEquals(true, life.checkNeighbor(liveCell_1_1, life.southOffset));
+        assertEquals(true, life.checkNeighbor(liveCell_1_1, life.eastOffset));
+        assertEquals(true, life.checkNeighbor(liveCell_1_1, life.westOffset));
+        assertEquals(true, life.checkNeighbor(liveCell_1_1, life.nwOffset));
+        assertEquals(true, life.checkNeighbor(liveCell_1_1, life.neOffset));
+        assertEquals(true, life.checkNeighbor(liveCell_1_1, life.swOffset));
+        assertEquals(true, life.checkNeighbor(liveCell_1_1, life.seOffset));
     }
 }
